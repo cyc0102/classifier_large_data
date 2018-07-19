31808ebed6884273ce534765c0b80c8557bba4c4
Bryan Chen <cyc0102@gmail.com>
2018-07-19 19:23:23 +0800

    predect for batch size = 10

--- predict_01.py
+++ predict_01.py
@@ -1,4 +1,3 @@
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 '''
 This is a demo python program to demo python and the important module for Image Recogniton using CNN
 
@@ -6,14 +5,16 @@ This is a demo python program to demo python and the important module for Image 
 Author: Bryan Chen
 
 '''
+
+
 import numpy as np
 
-i=1
+i=10
 Img_path = 'data/test2/'+str(i)+'.jpg'
 print (Img_path)
 
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-img = load_img(Img_path,target_size=(150,150))
+from keras.preprocessing.image import  img_to_array, load_img
+img = load_img(Img_path,target_size=(150,150)) # PIL image
 x = img_to_array(img)    # this is a Numpy array with shape ( Y, X , 3)
 print(x.shape)
 print(x.dtype)
@@ -26,15 +27,15 @@ fig.set_size_inches(10, 5)
 plt.imshow(x)             # RGB type 0~255 int or 0~1 float
 plt.show()
 
-x=x.reshape(1,150,150,3)
-print('Reshape = ', x.shape)
+x1=x.reshape(1,150,150,3)
+print('Reshape = ', x1.shape)
 
 
 from keras.models import load_model
 
 # 載入模型
 model = load_model('model_3.h5') # trained by large data
-prediction=model.predict(x)
+prediction=model.predict(x1)
 print('The prediction value is', prediction[0])
 if (prediction[0] > 0.5):  
     print('It is a dog image!')
@@ -44,5 +45,17 @@ else:
 import numpy as np
 x_4d=np.zeros((10,150,150,3),dtype=float)  # create 4 dimention ndarray with elements of zero
 print('Dimmentions =',x_4d.ndim) 
-x_4d[0]=x
-print(x_4d)
+# x_4d[0]=x
+# print(x_4d)
+
+index_str= input('Input the first index(1~12490) of test image :') #input is a str
+batch_size = 10
+for i in range (batch_size):
+    index= int(index_str) + i # transfer index type to int and plus i (0~9)
+    Img_path = 'data/test2/'+ str(index)  +'.jpg'
+    img = load_img(Img_path,target_size=(150,150)) 
+    x = img_to_array(img)    # this is a Numpy array with shape ( Y, X , 3)
+    x = x.astype('float32') / 255.0
+    x_4d[i]=x 
+prediction=model.predict(x_4d)
+print('The prediction value is', prediction[:10])
