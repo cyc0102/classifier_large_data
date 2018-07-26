fcd60d941a9c76c9a10d85fb40f247f9af4697f4
Bryan Chen <cyc0102@gmail.com>
2018-07-26 16:22:26 +0800

    update demo3

--- demo_03.py
+++ demo_03.py
@@ -16,66 +16,6 @@ model.summary()                         #print model summary
 
 label_dict={0:'cat', 1:'dog'}
 
-def plot_a_image(image,title,result):
-        fig = plt.gcf()
-        fig.set_size_inches(5, 6)       # set size
-        plt.imshow(x)                   # RGB type 0~255 int or 0~1 float
-        plt.title(title,fontsize=12)
-        plt.text(20,170,'The prediction result --> '+ result,fontsize=15)
-        plt.show()
-
-while 1:
-
-    i_str=input('Input Image (index:1~12500, out of range for exit):')
-    i=int(i_str)
-    if (i<1 or i>12500):
-        break
-    Img_path = 'data/test2/'+str(i)+'.jpg'
-    print('Imge file path:',Img_path)
- 
-    img_org = load_img(Img_path)                    # PIL image
-    print('The original image!')
-    img_org.show()                                  # Show the original image
-    img = load_img(Img_path,target_size=(150,150))  # PIL image, target_size=(150,150)
-    # print('The target_size image!')
-    # img.show()
-    x = img_to_array(img)                           # this is a Numpy array with shape ( Y, X , 3)
-    print('x.shape=',x.shape)
-    print('x.dtype',x.dtype)
-    x = x.astype('float32') / 255.0
-
-    x1=x.reshape(1,150,150,3)
-    print('x.reshape = ', x1.shape)
-
-
-    prediction=model.predict(x1)
-    print('The prediction value is', prediction[0])
-    if (prediction[0] > 0.5):  
-        print('It is a dog image!')
-    else:
-        print('It is a cat image!')  
-
-    prediction=np.rint(prediction)
-    print('np.rint(prediction)=',prediction)
-    print('prediction.shape=',prediction.shape)
-    print('predicton.dtype=',prediction.dtype)
-    prediction=prediction.astype(int)               #change Numpy array type
-    print('predicton after chagne type to int',prediction)
-    print('prediction.shape=',prediction.shape)
-    print('prediction.dtype=',prediction.dtype)
-    print('prediction[0]=',prediction[0])
-   
-
-    in_title='input Image:' + Img_path  
-
-    i=prediction[0]
-    print('i=',i)
-    i=int(i)                                        #change type to pyton int
-    print('int(i)=',i)
-    in_result=label_dict[i]
-    print('in_result=',in_result)
-    plot_a_image(x,in_title,in_result)
-
 
 
 batch_size = 12
@@ -97,16 +37,16 @@ def plot_12_images(images,prediction):
     plt.show()
 
 
-batch_size = 12
+
 while 1:
-    index_str= input('Input Images  (index:1~12489, batch_size=12, out of range for exit):') #input is a str
+    index_str= input('Input Images  (index:1~189, batch_size=12, out of range for exit):') #input is a str
     index=int(index_str)
-    if (index<1 or index>12489):
+    if (index<1 or index>189):
         break
 
     for i in range (batch_size):
         
-        Img_path = 'data/test2/'+ str(index)  +'.jpg'
+        Img_path = 'data/test3/'+ str(index)  +'.jpg'
         img = load_img(Img_path,target_size=(150,150)) 
         x = img_to_array(img)    # this is a Numpy array with shape ( Y, X , 3)
         x = x.astype('float32') / 255.0
@@ -115,7 +55,6 @@ while 1:
     
     prediction=model.predict(x_4d)
     prediction=np.rint(prediction)
-    print('The prediction value is', prediction[:batch_size])
     plot_12_images(x_4d,prediction)
 
 
